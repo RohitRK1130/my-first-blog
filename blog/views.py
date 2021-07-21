5b1d050d3c1198a1eac41c7b5ab75b8a0e1f9dc4
ROHIT KAMBLE <rohitkamble1130@gmail.com>
2021-07-22 00:47:44 +0530

    Added login,edit,newpost and same base.html file changed

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,35 @@ from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(Post,pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
 
 def post_new(request):
     if request.method == "POST":
@@ -16,7 +38,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -31,7 +53,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
